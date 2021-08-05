411080b2453333bc42458631779495a0ff18a2ef
JeanMatias <jcsmatias84@gmail.com>
2021-08-05 13:50:26 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         OMG. DOCKER RULES!!!! :) WOWOWOWOW
         </p>
         <a
           className="App-link"
