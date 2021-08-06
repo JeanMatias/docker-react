bec02a32a6b6dcda55810530d546a86edb5ac061
JeanMatias <jcsmatias84@gmail.com>
2021-08-06 10:51:34 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         OMG. DOCKER RULES!!!! :) WOWOWOWOW
+         OMG. DOCKER RULES!!!! :) WOWOWOWOW feature branch
         </p>
         <a
           className="App-link"
